5ba1c9cce18e014e95f211bd8ef09bee7d7e3261
aspitsin <alexs.spitsin@gmail.com>
2022-01-15 01:43:12 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0){
+        n = n *( -1); 
+    }
+    
+    let string = n.toString();
+    let res = '';
+
+    for(let i = string.length - 1; i >= 0; i-- ){
+        res += string[i];
+    }
+
+    return res;
 }
